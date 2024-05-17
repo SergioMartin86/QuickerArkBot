99a924b6251ad985da55e23d16514dbe7ac155c8
SergioMartin86 <eienburuu@gmail.com>
2024-05-17 19:13:28 +0200

    Uploading scaffold

---
+++ README.md
@@ -0,0 +1,15 @@
+QuickerArkBot
+---------------
+
+[![Build & Tests](https://github.com/SergioMartin86/quickerArkBot/actions/workflows/make.yml/badge.svg)](https://github.com/SergioMartin86/quickerArkBot/actions/workflows/make.yml)
+
+
+QuickerArkBot is a repository for testing and optimizing [ArkBot](https://github.com/sbroger/arkbot), an Arkanoid engine and solver. The main aim is to improve the performance of headless re-recording for TASing Arkanoid (NES) (See: [JaffarPlus](https://github.com/SergioMartin86/jaffarPlus)).
+  
+Credits
+---------
+
+- ArkBot was developed by [sbroger](https://github.com/sbroger) (ChefStef), under the Unlicense License
+
+All base code for this project was found under open source licenses, which I preserved in their corresponding files/folders. Any non-credited work is unintentional and shall be immediately rectfied.
+
---
+++ source/base/ArkBotInstance.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "../ArkBotInstanceBase.hpp"
+#include <string>
+#include <vector>
+#include <jaffarCommon/exceptions.hpp>
+#include <jaffarCommon/file.hpp>
+#include <jaffarCommon/serializers/base.hpp>
+#include <jaffarCommon/serializers/contiguous.hpp>
+#include <jaffarCommon/deserializers/base.hpp>
+
+namespace ark
+{
+
+class EmuInstance : public EmuInstanceBase
+{
+ public:
+
+  EmuInstance() : EmuInstanceBase()
+  {
+  }
+
+  ~EmuInstance()
+  {
+  }
+
+  virtual void initializeImpl(const std::string& gameDataPath) override
+  {
+  }
+
+  void initializeVideoOutput() override
+  {
+  }
+
+  void finalizeVideoOutput() override
+  {
+  }
+
+  void enableRendering() override
+  {
+  }
+
+  void disableRendering() override
+  {
+  }
+
+  void serializeState(jaffarCommon::serializer::Base& s) const override
+  {
+  }
+
+  void deserializeState(jaffarCommon::deserializer::Base& d) override
+  {
+  }
+
+  size_t getStateSizeImpl() const override
+  {
+    return 0;
+  }
+
+  void updateRenderer() override
+  {
+  }
+
+  inline size_t getDifferentialStateSizeImpl() const override { return 0; }
+
+  void enableStateBlockImpl(const std::string& block) override
+  {
+  }
+
+  void disableStateBlockImpl(const std::string& block) override
+  {
+  }
+
+  void doSoftReset() override
+  {
+  }
+  
+  void doHardReset() override
+  {
+  }
+
+  std::string getCoreName() const override { return "ArkBot"; }
+
+  uint8_t* getRamPointer() const override { return nullptr; }
+
+  void advanceStateImpl(ark::Controller controller) override
+  {
+  }
+
+  private:
+
+};
+
+} // namespace ark
